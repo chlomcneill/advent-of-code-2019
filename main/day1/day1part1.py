f17bb2e439155c87dc5c60106236434e709d4755
chlomcneill <c.mcneill@elsevier.com>
2019-12-04 10:52:58 +0000

    Day 1 both parts solved

---
+++ main/day1/day1part1.py
@@ -0,0 +1,24 @@
+f = open("/Users/mcneillc/Dev/my-stuff/advent-of-code-2019/inputs/day1input.txt", "r")
+masses = f.readlines()
+masses = [int(mass[:-1]) for mass in masses]
+f.close()
+
+
+# print(masses)
+
+
+def calculate_fuel(mass):
+    return int(mass / 3) - 2
+
+
+def main():
+    total_fuel = 0
+    for mass in masses:
+        total_fuel += calculate_fuel(mass)
+    return total_fuel
+
+
+print(main())
+
+
+
---
+++ main/day1/day1part2.py
@@ -0,0 +1,34 @@
+f = open("/Users/mcneillc/Dev/my-stuff/advent-of-code-2019/inputs/day1input.txt", "r")
+masses = f.readlines()
+masses = [int(mass[:-1]) for mass in masses]
+f.close()
+
+
+# print(masses)
+
+
+def calculate_fuel(mass):
+    return int(mass / 3) - 2
+
+
+def calculate_total_fuel(mass):
+    fuel = calculate_fuel(mass)
+    total_fuel = []
+    while fuel > 0:
+        total_fuel.append(fuel)
+        fuel = calculate_fuel(total_fuel[-1])
+    return sum(total_fuel)
+
+
+
+def main():
+    total_fuel = 0
+    for mass in masses:
+        total_fuel += calculate_total_fuel(mass)
+    return total_fuel
+
+
+print(main())
+
+
+
